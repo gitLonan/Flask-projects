fd2e77d917fc6a45d58460ab27392d3bd949caef
Lazar <sc2lazar@gmail.com>
2024-07-04 12:30:16 +0200

    css fixes

--- routes.py
+++ routes.py
@@ -16,21 +16,10 @@ def change_color():
 
 @app.route("/choose_character")
 def characters():
-    character1 = {
-        'name': 'Character 1',
-        'str': 10,
-        'agi': 8,
-        'int': 7,
-        'hp': 100,
-        'description': 'A brave warrior with unmatched strength.'
-    }
-    character2 = {
-        'name': 'Character 2',
-        'str': 8,
-        'agi': 9,
-        'int': 10,
-        'hp': 90,
-        'description': 'A cunning mage with great intellect.'
-    }
-    return render_template('character.html', title='Character Fight', character1=character1, character2=character2)
+    characters = [
+        {'name': 'Character 1', 'str': 10, 'agi': 8, 'int': 7, 'hp': 100, 'description': 'A brave warrior. aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'image': 'character1.png'},
+        {'name': 'Character 2', 'str': 7, 'agi': 10, 'int': 9, 'hp': 80, 'description': 'A swift rogue.', 'image': 'character2.png'},
+        # Add more characters here
+    ]
+    return render_template('character.html', title='Character Fight', characters=characters)
     
