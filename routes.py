2ff9c6769e2f18a56e82f550cbcf60b7f658b6bd
Lazar <sc2lazar@gmail.com>
2024-07-08 16:24:12 +0200

    fixed reroll

--- routes.py
+++ routes.py
@@ -16,6 +16,7 @@ def home_page():
     character.avaliable = True
     return render_template('main_screen.html', title='Home')
 
+
 @app.route("/character_creation", methods = ["GET", "POST"])
 def character_creation():
     """ When clicked 'create character' in the menu screen, redirects to the first phase, that is stats alocation """
@@ -23,14 +24,18 @@ def character_creation():
     if character.avaliable_random_gen == True:
         character.get_random_stats()
         character.avaliable_random_gen = False
+    
     text = non_combat_text.get_text('char_creation')
     
     return render_template('character_creation.html', character = character, text=text)
 
-@app.route('/update_stat/<stat>/<action>', methods=['POST'])
-def update_stat(stat, action): 
-    """ Handels increase and descreas of stats when creating character through HTTP POST method """
 
+
+@app.route('/update_stat/<stat>/<action>', methods=["GET", 'POST'])
+def update_stat(stat=None, action=None): 
+    """ Handels increase and descreas of stats when creating character through HTTP POST method """
+    
+    print(f"Received action: {action} for stat: {stat}")
     if action == "increment":
         if hasattr(character, stat):
             character.update(stat,action)
@@ -39,13 +44,18 @@ def update_stat(stat, action):
             character.update(stat, action)
     return redirect(url_for('character_creation'))
 
+@app.route('/reroll', methods=["POST"])
+def reroll():
+    """Handles rerolling stats."""
+    character.avaliable_random_gen = True
+    return redirect(url_for('character_creation'))
 
 @app.route("/choose_character")
 def characters():
     """
     Generate all the created characters and you can choose which you want to play
     """
-    
+
     characters = [
         {'name': 'Character 1', 'str': 10, 'agi': 8, 'int': 7, 'hp': 100, "wisdom": 9, 'description': 'A brave warrior from the east. Fighting to survive in this lands', 'image': 'character1.png'},
         {'name': 'Character 2', 'str': 7, 'agi': 10, 'int': 9, 'hp': 80, 'description': 'A swift rogue.', 'image': 'character2.png'},
--- stats_generator.py
+++ stats_generator.py
@@ -10,7 +10,7 @@ class Stats():
         self.HP = 0
         self.WISDOM = 0
 
-        self.POINTS = 4
+        self.POINTS = 0
         
         self.original_STRENGTH = 0
         self.original_AGILITY = 0
@@ -27,41 +27,35 @@ class Stats():
         self.INTELLIGENCE = random.randint(3,15)
         self.HP = random.randint(20,35)
         self.WISDOM = random.randint(3,15)
+        self.POINTS = 4
         self.setOriginalStats()
         
     def setOriginalStats(self):
-        self.original_STRENGTH += self.STRENGTH
-        self.original_AGILITY += self.AGILITY
-        self.original_INTELLIGENCE += self.INTELLIGENCE
-        self.original_HP += self.HP
-        self.original_WISDOM += self.WISDOM
+        self.original_STRENGTH = self.STRENGTH
+        self.original_AGILITY = self.AGILITY
+        self.original_INTELLIGENCE = self.INTELLIGENCE
+        self.original_HP = self.HP
+        self.original_WISDOM = self.WISDOM
         self.isSettingOriginalStats = False
 
     def update(self, stat, action):
         if action == "increment":
+            print("THIS IS INCREMENT",stat)
             if self.POINTS > 0:
                 setattr(self,stat, getattr(self, stat) + 1)
                 if stat == "HP":
                     setattr(self,stat, getattr(self, stat) + 4)
                 self.POINTS -= 1
+
         elif action == "decrement":
-            if stat == "STRENGTH" and getattr(self, stat) > self.original_STRENGTH:
-                setattr(self,stat, getattr(self, stat) - 1)
+            original_value = getattr(self, f"original_{stat}")
+            current_value = getattr(self, stat)
+            if stat in ["STRENGTH", "AGILITY", "INTELLIGENCE", "WISDOM"] and current_value > original_value:
+                print("THIS IS DECREMENT",stat)
+                setattr(self, stat, current_value - 1)
                 self.POINTS += 1
-            elif stat == "AGILITY" and getattr(self, stat) > self.original_AGILITY:
-                setattr(self,stat, getattr(self, stat) - 1)
-                self.POINTS += 1
-            elif stat == "INTELLIGENCE" and getattr(self, stat) > self.original_INTELLIGENCE:
-                setattr(self,stat, getattr(self, stat) - 1)
-                self.POINTS += 1
-            elif stat == "INTELLIGENCE" and getattr(self, stat) > self.original_INTELLIGENCE:
-                setattr(self,stat, getattr(self, stat) - 1)
-                self.POINTS += 1
-            elif stat == "HP" and getattr(self, stat) > self.original_HP:
-                setattr(self,stat, getattr(self, stat) - 5)
-                self.POINTS += 1
-            elif stat == "WISDOM" and getattr(self, stat) > self.original_WISDOM:
-                setattr(self,stat, getattr(self, stat) - 1)
+            elif stat == "HP" and current_value > original_value:
+                setattr(self, stat, current_value - 5)
                 self.POINTS += 1
             
 
