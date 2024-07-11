fda4f96c2cf7974cd90e67ac42ae19a58e3ca33f
Lazar <sc2lazar@gmail.com>
2024-07-11 19:46:17 +0200

    .

--- routes.py
+++ routes.py
@@ -3,11 +3,11 @@ from flask import render_template, redirect, url_for, request
 from app import character
 from app import non_combat_text
 import typing
+import random
 
 
 
-
-
+############ MAIN SCREEN ##################################
 @app.route("/main_screen")
 @app.route("/", methods = ["GET", "POST"])
 def home_page():
@@ -15,21 +15,22 @@ def home_page():
 
     character.avaliable = True
     return render_template('main_screen.html', title='Home')
+##########################
 
 
+############ Character Creation Related ##################################
 @app.route("/character_creation", methods = ["GET", "POST"])
 def character_creation():
     """ When clicked 'create character' in the menu screen, redirects to the first phase, that is stats alocation """
 
     if character.avaliable_random_gen == True:
         character.get_random_stats()
+        character.apply_difficulty()
         character.avaliable_random_gen = False
     
     text = non_combat_text.get_text('char_creation')
     
     return render_template('character_creation.html', character = character, text=text)
-
-
 
 @app.route('/update_stat/<stat>/<action>', methods=["GET", 'POST'])
 def update_stat(stat=None, action=None): 
@@ -49,7 +50,25 @@ def reroll():
     """Handles rerolling stats."""
     character.avaliable_random_gen = True
     return redirect(url_for('character_creation'))
+##############################################################
 
+############ Set difficulty  #################################
+@app.route('/select_difficulty', methods=["POST", "GET"])
+def select_difficulty():
+    if request.method == "GET":
+        tips = non_combat_text.get_text('tips')
+    if request.method == "POST":
+        tips = non_combat_text.text
+        character.current_difficulty = request.form.get('difficulty')
+
+    return render_template('difficulty_menu.html', selected_difficulty = character.current_difficulty, tip_content = tips)
+
+@app.route("/next_tip", methods=["POST"])
+def next_tip():
+    if request.method == "POST":
+        tips = non_combat_text.get_text('tips')
+    return redirect(url_for("select_difficulty"))
+########################################################
 @app.route("/choose_character")
 def characters():
     """
--- stats_generator.py
+++ stats_generator.py
@@ -1,5 +1,5 @@
 import random
-
+import typing
 
 class Stats():
     def __init__(self):
@@ -17,12 +17,59 @@ class Stats():
         self.original_INTELLIGENCE = 0
         self.original_HP = 0
         self.original_WISDOM = 0
+        self.stats = {          "STRENGTH": "original_STRENGTH",
+                                "AGILITY": "original_AGILITY",
+                                "INTELLIGENCE": "original_INTELLIGENCE",
+                                "HP": "original_HP",
+                                "WISDOM": "original_WISDOM"
+                                }
+        self.difficulty = {"Baby": {
+                                "STRENGTH": 3,
+                                "AGILITY": 3,
+                                "INTELLIGENCE": 3,
+                                "HP": 10,
+                                "WISDOM": 3
+                                },
+                            "Easy": {
+                                "STRENGTH": 1,
+                                "AGILITY": 1,
+                                "INTELLIGENCE": 1,
+                                "HP": 5,
+                                "WISDOM": 1
+                                },
+                            "Normal": {
+                                "STRENGTH": 0,
+                                "AGILITY": 0,
+                                "INTELLIGENCE": 0,
+                                "HP": 0,
+                                "WISDOM": 0
+                                },
+                            "Hard": {
+                                "STRENGTH": -1,
+                                "AGILITY": -1,
+                                "INTELLIGENCE": -1,
+                                "HP": -5,
+                                "WISDOM": -1
+                            }
+        }
+        self.current_difficulty = "Baby"
         
         self.avaliable_random_gen = True
         self.isSettingOriginalStats = True
 
-    def get_random_stats(self):
-        self.STRENGTH = random.randint(3,15)
+    def apply_difficulty(self) -> None:
+        """ Based on the difficulty values are added to the stats and then original stats are set again"""
+        for stat_name,stat_value in self.difficulty[f'{self.current_difficulty}'].items():
+            original_value = getattr(self, stat_name)
+            for i in self.stats.keys():
+                if i == stat_name: 
+                    setattr(self, stat_name, original_value + stat_value) 
+                    break
+        self.setOriginalStats()
+
+
+    def get_random_stats(self) -> None:
+        self.STRENGTH = random.randint(3,15) 
         self.AGILITY = random.randint(3,15)
         self.INTELLIGENCE = random.randint(3,15)
         self.HP = random.randint(20,35)
@@ -30,7 +77,7 @@ class Stats():
         self.POINTS = 4
         self.setOriginalStats()
         
-    def setOriginalStats(self):
+    def setOriginalStats(self) -> None:
         self.original_STRENGTH = self.STRENGTH
         self.original_AGILITY = self.AGILITY
         self.original_INTELLIGENCE = self.INTELLIGENCE
@@ -39,6 +86,7 @@ class Stats():
         self.isSettingOriginalStats = False
 
     def update(self, stat, action):
+        """ Deals with adding and descreasing stats in character creation window """
         if action == "increment":
             print("THIS IS INCREMENT",stat)
             if self.POINTS > 0:
@@ -57,5 +105,7 @@ class Stats():
             elif stat == "HP" and current_value > original_value:
                 setattr(self, stat, current_value - 5)
                 self.POINTS += 1
-            
+
+
+        
 
